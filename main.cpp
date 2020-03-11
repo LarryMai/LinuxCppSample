54727ff2e9bf84b7fdda5ffed62a7b11f173a4e5
sam_chan <sam3chan@gmail.com>
2020-03-11 19:54:56 +0800

    範例程式

--- main.cpp
+++ main.cpp
@@ -1,49 +1,61 @@
-#include<iostream>
-#include <string>
-#include <vector>
+#include <iostream>
+//#include <string>
+//#include <vector>
 #include <regex>
-#include <memory>
-#include "DataAccessor.hpp"
+//#include <memory>
+//#include "DataAccessor.hpp"
+
 using namespace std;
-using namespace SampleProject;
-int main(int argc, char const *argv[])
+//using namespace SampleProject;
+
+int main( int argc, char const *argv[ ] )
 {
-    /* code
-     */
+/*	int count = 10;
+	for( int i=0; i < count; ++i )
+	{
+		printf( "[%d] = %d\n", i, (i+1)*(i+1) );
+	} //依序印出 1 ~ 10 的平方
+*/
+    std::string  pattern = "([A-Za-z]+)([0-9]+)"; //準備轉換成正規式物件
+    std::string  input   = "KKKtry23";
+    regex  reg( pattern ); //前方為英文字母,後方為數字 <--用兩組小括號設定了分割點
 
-    int count = 10;
-
-    for(int i=0;i< count ;++i)
+    smatch  sm;
+    if( regex_match( input, sm, reg ) )
     {
-        printf("[%d] = %d\n", i , (i+1)*(i+1) );
-    }
-
-    std::string pattern = "([A-Za-z]+)([0-9]+)";
-    std::string input = "test123";
-    regex reg(pattern);
-
-    smatch sm;
-    if(regex_match(input,sm,reg))
-    {
-       for(int i=0; i< sm.size();++i)
+       for( int i=0; i < sm.size( ); ++i )
        {
-           std::string value = sm[i];
-           std::cout << value << i <<  std::endl;
+           std::string  value = sm[ i ];
+           std::cout << "sm[ " << i << " ] = " << value << "  <-- i = " << i <<  std::endl;
        }
     }
  
-    std::shared_ptr<DataAccessor> dataHolder;
-    dataHolder.reset(new DataAccessor);
+/*    std::shared_ptr<DataAccessor> dataHolder;
+    dataHolder.reset( new DataAccessor );
 
-    if(dataHolder->Initialize())
+    if( dataHolder->Initialize( ) )
     {
-       auto users =  dataHolder->GetUsers();
-       for(int i =0; i< users.size();++i)
+       auto users =  dataHolder->GetUsers( );
+       for( int i=0; i < users.size( ); ++i )
        {
-           const char * tempStr = users[i].Name.c_str();
+           const char*  tempStr = users[ i ].Name.c_str( );
            std::cout << tempStr << std::endl;
        }
     }
     std::cout << "Hello World" << std::endl;
-    return 0;
+*/    return 0;
 }
+
+/*
+$ g++ main.cpp -o main
+$ ./main
+sm[ 0 ] = test123  <-- i = 0
+sm[ 1 ] = test  <-- i = 1
+sm[ 2 ] = 123  <-- i = 2   由此得出 sm.size( ) = 3
+$ g++ main.cpp -o main
+$ ./main
+sm[ 0 ] = KKKtry23  <-- i = 0 <--格式沒變
+sm[ 1 ] = KKKtry  <-- i = 1
+sm[ 2 ] = 23  <-- i = 2
+$　若 input 字串的格式不符合,就篩不到 sm
+*/
